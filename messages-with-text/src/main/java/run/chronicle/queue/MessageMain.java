f4380a5e4868b90fda8f87ad6f7c34d47d9c989e
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2021-01-25 10:22:23 +0000

    Add a demo for writing and reading messages containing text. #5

--- messages-with-text/src/main/java/run/chronicle/queue/MessageMain.java
+++ messages-with-text/src/main/java/run/chronicle/queue/MessageMain.java
@@ -9,8 +9,9 @@ public class MessageMain {
     private static final int COUNT = Integer.getInteger("count", 10_000_000);
 
     public static void main(String[] args) {
-        for (int t = 0; t < 100; t++) {
-            String test = "test-" + System.nanoTime();
+        for (int t = 0; t < 5; t++) {
+            long start = System.nanoTime();
+            String test = "test-" + start;
             try (ChronicleQueue queue = ChronicleQueue.single(test)) {
                 Messages messages = queue.methodWriter(Messages.class);
                 Message message = new Message();
@@ -25,7 +26,8 @@ public class MessageMain {
                 MethodReader reader = queue.createTailer().methodReader(messagesCounter);
                 while (reader.readOne()) {
                 }
-                System.out.println("Read " + count[0] + " of " + COUNT + " messages");
+                long time = System.nanoTime() - start;
+                System.out.printf("Read %,d of %,d messages in %.3f seconds%n", count[0], COUNT, time / 1e9);
             }
             IOTools.deleteDirWithFiles(test);
         }
