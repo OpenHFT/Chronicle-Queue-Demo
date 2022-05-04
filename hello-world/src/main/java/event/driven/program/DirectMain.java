c7d67c963de600e3725d8a9940000be5546b46d3
forough.goudarzi <forough.Goudarzi@chronicle.software>
2022-05-04 11:21:46 +0100

    Add Event driven example. Fix #10.

--- hello-world/src/main/java/event/driven/program/DirectMain.java
+++ hello-world/src/main/java/event/driven/program/DirectMain.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2021 - http://chronicle.software
+ *
+ * Chronicle software holds the rights to this software and it may not be redistributed to another organisation or a different team within your organisation.
+ *
+ * You may only use this software if you have prior written consent from Chronicle Software.
+ *
+ * This written consent may take the form of a valid (non expired) software licence.
+ */
 package event.driven.program;
 
 import java.io.IOException;
