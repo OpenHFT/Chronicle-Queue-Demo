1175850ddf55fe409b257d4ef99b6a8591d32299
Peter Lawrey <peter.lawrey@chronicle.software>
2022-05-11 14:48:17 +0100

    Break into two lines

--- hello-world/src/main/java/event/driven/program/RecordInputMain.java
+++ hello-world/src/main/java/event/driven/program/RecordInputMain.java
@@ -16,7 +16,9 @@ import net.openhft.chronicle.wire.Wires;
 
 public class RecordInputMain {
     public static void main(String[] args) throws IOException {
+        // obtains a proxy that writes to the PrintStream the method calls and their arguments
         final Says says = Wires.recordAsYaml(Says.class, System.out);
+        // Takes each line input and calls say(theLine) each time
         SaysInput.input(says);
     }
 }
