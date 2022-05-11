143f4a24ba6f52694cea4cdd43c2967aefd7b92e
forough.goudarzi <forough.Goudarzi@chronicle.software>
2022-05-11 14:35:33 +0100

    update.

---
+++ hello-world/src/main/java/event/driven/program/RecordInputMain.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021 - http://chronicle.software
+ *
+ * Chronicle software holds the rights to this software and it may not be redistributed to another organisation or a different team within your organisation.
+ *
+ * You may only use this software if you have prior written consent from Chronicle Software.
+ *
+ * This written consent may take the form of a valid (non expired) software licence.
+ */
+package event.driven.program;
+
+import event.driven.program.api.Says;
+
+import java.io.IOException;
+import net.openhft.chronicle.wire.Wires;
+
+public class RecordInputMain {
+  //  private static SaysOutput c1 = new SaysOutput();
+    public static void main(String[] args) throws IOException {
+      //System.out.
+
+          SaysInput.input(Wires.recordAsYaml(Says.class, System.out));
+    }
+}
