8d3dd68f2f217cff44d6dd4218c2154ff14dec4a
Keiran Lawrey <keiran.lawrey@chronicle.software>
2023-07-29 14:02:11 +0100

    Added Javadoc

--- simple-translator/src/main/java/net/openhft/chronicle/queue/simple/translator/InputMain.java
+++ simple-translator/src/main/java/net/openhft/chronicle/queue/simple/translator/InputMain.java
@@ -6,21 +6,39 @@ import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import java.util.Scanner;
 
 /**
+ * A Main class that takes user input from the console and writes it to a queue ("queue-en").
+ * Continues to accept input until an empty line is entered by the user.
+ *
  * Created by catherine on 26/07/2016.
  */
 public class InputMain {
     public static void main(String[] args) {
+        // Path to the queue where input will be stored
         String path_en = "queue-en";
+
+        // Create a SingleChronicleQueue for storing the input
         SingleChronicleQueue queue_en = SingleChronicleQueueBuilder.binary(path_en).build();
+
+        // Create a MessageConsumer that will append messages to the queue
         MessageConsumer messageConsumer = queue_en.acquireAppender().methodWriter(MessageConsumer.class);
+
+        // Create a Scanner to read input from the console
         Scanner read = new Scanner(System.in);
+
+        // Continuously read input from the console and write it to the queue
         while (true) {
             System.out.println("type something");
             String line = read.nextLine();
+
+            // Exit the loop if the input line is empty
             if (line.isEmpty())
                 break;
+
+            // Write the input to the queue
             messageConsumer.onMessage(line);
         }
+
+        // Inform the user that the program has finished
         System.out.println("... bye.");
     }
 }
--- simple-translator/src/main/java/net/openhft/chronicle/queue/simple/translator/TranslatorMain.java
+++ simple-translator/src/main/java/net/openhft/chronicle/queue/simple/translator/TranslatorMain.java
@@ -5,23 +5,42 @@ import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 
 /**
+ * A Main class that sets up a translator for messages between two queues.
+ * This class reads messages from an English queue, translates them into French,
+ * and then writes the translated messages into a French queue.
+ *
  * Created by catherine on 26/07/2016.
  */
 public class TranslatorMain {
     public static void main(String[] args) throws InterruptedException {
+        // Path to the queue for French messages
         String pathfr = "queue-fr";
+
+        // Create a SingleChronicleQueue for French messages
         SingleChronicleQueue queuefr = SingleChronicleQueueBuilder.binary(pathfr).build();
+
+        // Create a MessageConsumer that will append messages to the French queue
         MessageConsumer messageConsumer = queuefr.acquireAppender().methodWriter(MessageConsumer.class);
 
+        // Create a SimpleTranslator that will translate messages to French
         MessageConsumer simpleTranslator = new SimpleTranslator(messageConsumer);
 
+        // Path to the queue for English messages
         String path_en = "queue-en";
+
+        // Create a SingleChronicleQueue for English messages
         SingleChronicleQueue queue_en = SingleChronicleQueueBuilder.binary(path_en).build();
+
+        // Create a MethodReader that will read messages from the English queue
         MethodReader methodReader = queue_en.createTailer().methodReader(simpleTranslator);
 
+        // Continuously read and translate messages from the English queue to the French queue
         while (true) {
+            // Read and translate one message, if available
+            // If no message was available, pause for a short time to avoid busy-waiting
             if (!methodReader.readOne())
                 Thread.sleep(10);
         }
     }
 }
+
